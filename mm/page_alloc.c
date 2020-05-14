e1292e29a9230e1569fc4d5f8e92366de69290fc
spacetree1 <klepikov8561@gmail.com>
2020-05-14 23:14:48 +0300

    Revert "simple_lmk: Introduce Simple Low Memory Killer for Android"
    Revert "mm: vmpressure: Ignore allocation orders above PAGE_ALLOC_COSTLY_ORDER"
    
    This reverts commit 6e73060765dcf7f4cc09a33ab2ac0bfefde5303c.
    
    Revert "arm64: configs: Use Simple LMK"
    
    This reverts commit 6b98bc0d114f595efac767edd90840525f0e8918.
    
    Revert "mm: Don't warn on page allocation failures for OOM-killed processes"
    
    This reverts commit 89b2671bf9368e6215e4f56c9889288f623bd3d7.
    
    Revert "mm: vmpressure: Don't cache the window size"
    
    This reverts commit 2d8d928279155a95309b9883b85d3f825eeedc7c.
    
    Revert "mm: vmpressure: Interpret zero scanned pages as 100% pressure"
    
    This reverts commit f080303b14479e56e823b570950fa0eeb4b47d8a.
    
    Revert "mm: vmpressure: Don't exclude any allocation types"
    
    This reverts commit 38ee701767dc01bdae7c4cb40a6b65ef2d96be51.
    
    Revert "simple_lmk: Update adj targeting for Android 10"
    
    This reverts commit f288dd242d738d742b88a68560e8149b10f85765.
    
    Revert "simple_lmk: Use vmpressure notifier to trigger kills"
    
    This reverts commit 8020023c9f2b53e566bb00446bf9bd1f78d0610e.
    
    Revert "mm: Stop kswapd early when nothing's waiting for it to free pages"
    
    This reverts commit 04af8c7fc9126b8be9ddd15dab2404a4d44f3cc1.
    
    Revert "simple_lmk: Include swap memory usage in the size of victims"
    
    This reverts commit c9222e04cbe1f83f74446ae7edceca9be27d3c31.
    
    Revert "simple_lmk: Relax memory barriers and clean up some styling"
    
    This reverts commit 4df4bf2c0ad113d505765144409333d5e3e73510.
    
    Revert "simple_lmk: Place victims onto SCHED_RR"
    
    This reverts commit 608ce3107a87a7162e1d383353a8c1bab56210ae.
    
    Revert "simple_lmk: Add a timeout to stop waiting for victims to die"
    
    This reverts commit 5cae405cb1851f9434c1bb3d66b8024f36bbb8a1.
    
    Revert "simple_lmk: Ignore tasks that won't free memory"
    
    This reverts commit be75896f18c7e32a60ccd3f77ddf8d59bb700977.
    
    Revert "simple_lmk: Simplify tricks used to speed up the death process"
    
    This reverts commit 5ef6e64c3899ff7b3fa8584cb3a011785ec0ec2b.
    
    Revert "simple_lmk: Report mm as freed as soon as exit_mmap() finishes"
    
    This reverts commit 662136e748470f4e05ad76b7e7a465b5b7f18c4f.
    
    Revert "simple_lmk: Mark victim thread group with TIF_MEMDIE"
    
    This reverts commit 05fa6a0a2efae4f5010a0cdcdc14c299e31ec0f9.
    
    Revert "simple_lmk: Disable OOM killer when Simple LMK is enabled"
    
    This reverts commit 2174c358051cce83e898fe79ae3cef7a27ae7715.
    
    Revert "simple_lmk: Print a message when there are no processes to kill"
    
    This reverts commit 33c91a8760c24350f54d399febdc2586ebd7fdb5.
    
    Revert "simple_lmk: Remove compat cruft not specific to 4.9"
    
    This reverts commit 374e2940339c064d884b8d459a4f7508d2a4fda6.
    
    Revert "simple_lmk: Update copyright to 2020"
    
    This reverts commit 22788774bc3fb04feb7decfd4643080d1dd13118.
    
    Revert "simple_lmk: Don't queue up new reclaim requests during reclaim"
    
    This reverts commit 051d966b962008e32350e48ddb23c8d0cde08323.
    
    Revert "simple_lmk: Increase default minfree value"
    
    This reverts commit 3409aa083e58b541ce20036b52338d9782f4acf3.
    
    Revert "simple_lmk: Clean up some code style nitpicks"
    
    This reverts commit b56d2ac390ad7715849db31c48d4ee2f4b37f3d6.
    
    Revert "simple_lmk: Make reclaim deterministic"
    
    This reverts commit 0b20615bdb5087a7aa1d3af0281d2fbbc0caae8d.
    
    Revert "simple_lmk: Fix broken multicopy atomicity for victims_to_kill"
    
    This reverts commit c39d9b860f446354a2cca62b2720fdbd8c6ce3f7.
    
    Revert "simple_lmk: Use proper atomic_* operations where needed"
    
    This reverts commit 8027489ba4096eb94a690dc622d9a0440b2e6558.
    
    Revert "simple_lmk: Remove kthread_should_stop() exit condition"
    
    This reverts commit c42ce4317cea038a139c9602f6915672883883aa.
    
    Revert "simple_lmk: Fix pages_found calculation"
    
    This reverts commit 08b40e156fa84561680cb28f35093abb7d848eb2.
    
    Revert "simple_lmk: Introduce Simple Low Memory Killer for Android"
    
    This reverts commit 47e2179e3cbf37800f505cc9e0a608dd506a0475.

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -3676,8 +3676,6 @@ __alloc_pages_slowpath(gfp_t gfp_mask, unsigned int order,
 	int compaction_retries;
 	int no_progress_loops;
 	unsigned int cpuset_mems_cookie;
-	pg_data_t *pgdat = ac->preferred_zoneref->zone->zone_pgdat;
-	bool woke_kswapd = false;
 
 	/*
 	 * In the slowpath, we sanity check order to avoid ever trying to
@@ -3722,13 +3720,8 @@ retry_cpuset:
 	 */
 	alloc_flags = gfp_to_alloc_flags(gfp_mask);
 
-	if (gfp_mask & __GFP_KSWAPD_RECLAIM) {
-		if (!woke_kswapd) {
-			atomic_inc(&pgdat->kswapd_waiters);
-			woke_kswapd = true;
-		}
+	if (gfp_mask & __GFP_KSWAPD_RECLAIM)
 		wake_all_kswapds(order, ac);
-	}
 
 	/*
 	 * The adjusted alloc_flags might result in immediate success, so try
@@ -3827,10 +3820,8 @@ retry:
 	}
 
 	/* Avoid allocations with no watermarks from looping endlessly */
-	if (test_thread_flag(TIF_MEMDIE) && !(gfp_mask & __GFP_NOFAIL)) {
-		gfp_mask |= __GFP_NOWARN;
+	if (test_thread_flag(TIF_MEMDIE) && !(gfp_mask & __GFP_NOFAIL))
 		goto nopage;
-	}
 
 
 	/* Try direct reclaim and then allocating */
@@ -3902,12 +3893,9 @@ nopage:
 	if (read_mems_allowed_retry(cpuset_mems_cookie))
 		goto retry_cpuset;
 
+	warn_alloc(gfp_mask,
+			"page allocation failure: order:%u", order);
 got_pg:
-	if (woke_kswapd)
-		atomic_dec(&pgdat->kswapd_waiters);
-	if (!page)
-		warn_alloc(gfp_mask,
-				"page allocation failure: order:%u", order);
 	return page;
 }
 
@@ -5966,7 +5954,6 @@ static void __paginginit free_area_init_core(struct pglist_data *pgdat)
 	pgdat_page_ext_init(pgdat);
 	spin_lock_init(&pgdat->lru_lock);
 	lruvec_init(node_lruvec(pgdat));
-	pgdat->kswapd_waiters = (atomic_t)ATOMIC_INIT(0);
 
 	for (j = 0; j < MAX_NR_ZONES; j++) {
 		struct zone *zone = pgdat->node_zones + j;
